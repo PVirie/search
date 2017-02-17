ec88836c76aea76ff022a872eb192e4706127f3c
Tao <tao@thebitstudio.com>
2017-02-17 12:02:01 +0700

    Revert "merged"
    This reverts commit 9f10b8983029d9ca6743f25dcb4972f0754d6e0b, reversing
    changes made to 3a620f95c12105beb0bce4fce085eeddc3daae8c.

--- src/test.py
+++ src/test.py
@@ -3,17 +3,15 @@ import network as ann
 import cv2
 
 
-templates, examples = gen.gen_batch(1, input_size=(20, 20), output_size=(50, 50))
+templates, examples = gen.gen_batch(2, input_size=(20, 20), output_size=(50, 50))
 
 print "Figures >>", templates.shape
 print "Examples >>", examples.shape
 
-nn = ann.Network(1, (20, 20), (50, 50), 25)
+nn = ann.Network(2, (20, 20), (50, 50), 20)
 nn.load_session("../artifacts/" + "test_weight")
-drawn, output = nn.draw(templates, examples)
-print nn.debug(templates, examples)
+drawn = nn.draw(templates, examples)
 
-print output[0]
 cv2.imshow("template", templates[0])
 cv2.imshow("example", examples[0])
 cv2.imshow("drawn", drawn[0])
